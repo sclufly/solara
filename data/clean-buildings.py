f6dc519963756053ea78da9609d6fa9f6cc3282c
Sarah Cloughley <smcloughley@gmail.com>
2026-06-25 13:49:25 -0400

    improved efficiency of shadow cluster test

--- data/clean-buildings.py
+++ data/clean-buildings.py
@@ -1,13 +1,13 @@
 import geopandas as gpd
 import pybdshadow
 
-buildings = gpd.read_file("./data/buildings/buildings.shp")
+buildings = gpd.read_file("./3DMassingShapefile_2025_WGS84/3DMassingShapefile_2025_WGS84.shp")
 
 buildings = buildings.to_crs(epsg=4326)
-buildings["height"] = buildings["MAX_HEIGHT"]
+buildings["height"] = buildings[["MAX_HEIGHT", "AVG_HEIGHT"]].max(axis=1)
 buildings = pybdshadow.bd_preprocess(buildings)
 
 buildings.to_file(
-    "./data/buildings/buildings_preprocessed.gpkg",
+    "./buildings/buildings.gpkg",
     driver="GPKG"
 )
--- shadows.py
+++ shadows.py
@@ -2,33 +2,65 @@ import pandas as pd
 import geopandas as gpd
 import pybdshadow
 import matplotlib.pyplot as plt
+import numpy as np
 
-buildings = gpd.read_file(r"./data/buildings/buildings_preprocessed.gpkg")
+DATA_PATH = r"./data/buildings/buildings.gpkg"
 
-# select sample cluster of buildings
-cluster_size = 100
-seed_building = buildings.sample(1, random_state=0)
-seed_centroid = seed_building.geometry.iloc[0].centroid
-centroids = buildings.geometry.centroid
-nearest_idx = centroids.distance(seed_centroid).nsmallest(cluster_size).index
-buildings = buildings.loc[nearest_idx]
+# select a cluster of buildings within a local area of interest (AOI)
+def select_cluster_from_aoi(data_path, cluster_size=500, initial_radius_m=1200, max_radius_m=20000):
 
-# pre-process buildings
-# buildings = buildings.to_crs(epsg=4326)
-# buildings["height"] = buildings["MAX_HEIGHT"]
-# buildings = pybdshadow.bd_preprocess(buildings)
+    # select a seed building
+    seed_building = gpd.read_file(data_path, rows=slice(138194, 138195))
+    
+    seed_centroid = seed_building.geometry.iloc[0].centroid
+    seed_x, seed_y = seed_centroid.x, seed_centroid.y
+    radius_m = initial_radius_m
+    buildings = gpd.GeoDataFrame()
 
-print(buildings.geometry.name)
-print(buildings.head(5))
+    # expand AOI until we have enough candidates
+    while radius_m <= max_radius_m:
+        # approx meter-to-degree conversion for WGS84 readtime bbox
+        dlat = radius_m / 111320.0
+        dlon = radius_m / (111320.0 * max(0.1, abs(np.cos(np.radians(seed_y)))))
+        bbox = (seed_x - dlon, seed_y - dlat, seed_x + dlon, seed_y + dlat)
+
+        buildings = gpd.read_file(data_path, bbox=bbox)
+        if len(buildings) >= cluster_size:
+            break
+
+        radius_m *= 2
+
+    if len(buildings) == 0:
+        raise ValueError("AOI query returned no buildings")
+
+    # project only local AOI and use vectorized centroid distance in meters
+    metric_crs = buildings.estimate_utm_crs()
+    buildings_metric = buildings.to_crs(metric_crs)
+    seed_metric = gpd.GeoSeries([seed_centroid], crs=seed_building.crs).to_crs(metric_crs).iloc[0]
+
+    centroids = buildings_metric.geometry.centroid
+    dx = centroids.x - seed_metric.x
+    dy = centroids.y - seed_metric.y
+    buildings_metric["dist2"] = dx * dx + dy * dy
+
+    cluster = buildings_metric.nsmallest(min(cluster_size, len(buildings_metric)), "dist2")
+    cluster = cluster.drop(columns=["dist2"]).to_crs(buildings.crs)
+
+    return cluster
+
+# select sample cluster of buildings with AOI query
+cluster_size = 2500
+buildings = select_cluster_from_aoi(DATA_PATH, cluster_size=cluster_size)
+
+print("== Cluster selection complete, creating shadows... ==")
 
 # create the shadows
-date = pd.to_datetime('2022-06-01 12:45:33.959797119')\
-    .tz_localize('America/Toronto')
+date = pd.to_datetime('2022-11-01 16:45:33.959797119').tz_localize('America/Toronto')
 
 shadows = pybdshadow.bdshadow_sunlight(
     buildings,
     date,
-    roof=True,
+    roof=False,
     include_building=False
 )
 
